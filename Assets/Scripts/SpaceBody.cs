697825ef41687367a0bf7684b28779e4a3cfc79f
ITsyman <mr.tsyman@mail.ru>
2020-02-08 23:20:45 +0400

    Base logic for planet rotation

---
+++ Assets/Scripts/SpaceBody.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class SpaceBody : MonoBehaviour
+{
+    // Угловая скорость собсвенного вращения тела
+    public float selfRotationSpeed;
+
+    // Угловая скорость вращения на орбите 
+    public float orbitRotationSpeed;
+
+    // Центр орбиты
+    public Transform centerOfRotation;
+
+    // Радиус вектор от центра вращения до тела
+    private Vector3 radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void FixedUpdate() 
+    {
+        RotateAbouSelfAxis();
+
+        RotateAboutCenter();
+    }
+
+    private void RotateAbouSelfAxis()
+    {
+        transform.rotation *= Quaternion.Euler(0, selfRotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void RotateAboutCenter()
+    {
+        radius = centerOfRotation.position - transform.position;
+        
+        // Вектор угловой скорости вращения
+        Vector3 omega = Vector3.ProjectOnPlane(transform.up, radius).normalized * orbitRotationSpeed * Time.deltaTime;
+        // Линейная скорость
+        Vector3 linearVelocity = Vector3.Cross(radius, omega);
+        // Конечное пермещение за 1 секунду
+        Vector3 offset = linearVelocity * Time.deltaTime;
+
+        transform.position += offset;
+    }
+}
