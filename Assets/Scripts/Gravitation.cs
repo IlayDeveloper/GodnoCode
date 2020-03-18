221f8124bdbe80f0721cd09afab487185c26cc87
Roman <rnenashev@smuit.ru>
2020-03-18 14:18:27 +0400

    Step5

--- Assets/Scripts/Gravitation.cs
+++ Assets/Scripts/Gravitation.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Gravitation : MonoBehaviour
 {
-    [SerializeField] private float g=;
+    [SerializeField] private float g=9.8f;
     private float mass;
     
     // Update is called once per frame
     void Update()
     {
         float shipMass = SpaceShip.Instance.mass;
-        float force =  g * (mass * SpaceShip.Instance.mass)/()
+        //float force =  g * (mass * SpaceShip.Instance.mass)/()
     }
 
     private void Start()
